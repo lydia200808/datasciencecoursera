897445939acea45380077e7580233fb2678e545b
Lydia <lixizi1992@gmail.com>
2014-11-23 14:24:14 -0800

    version 1

--- cachematrix.R
+++ cachematrix.R
@@ -1,18 +1,60 @@
-## Put comments here that give an overall description of what your
-## functions do lala
+## A pair of functions that cache the inverse of a matrix
 
-## Write a short comment describing this function
+## makeCacheMatrix function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # initialize the variable storing 'inverse' of x 
+        i <- NULL
+        
+        # method to set/reset an input matrix
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        
+        # method to get an input matrix
+        get <- function() x
+        
+        # method to set the inverse of matrix
+        setinverse <- function(inverse) i <<- inverse
+        
+        # method to get the inverse of matrix
+        getinverse <- function() i
+        
+        # return a list of all methods
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve function computes the inverse of the special "matrix" returned 
+## by `makeCacheMatrix` above. If the inverse has already been 
+## calculated (and the matrix has not changed), then `cacheSolve` should 
+## retrieve the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(z, ...) {
+        
+        # fetch inverse of matrix 'x' which is input of function makeCacheMatrix
+        i <- z$getinverse()
+        
+        # return the inverse if it's already computed
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        
+        # get input matrix 'x'
+        TempMat <- z$get()
+        
+        # compute the inverse of 'x'
+        i <- solve(TempMat, ...)
+        
+        # cache this inverse in third element of z
+        z$setinverse(i)
+        
+        # return inverse matrix
+        i
 }
 
 
-~000
